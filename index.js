8da2fd31d395fb46ccc98ad1eb08f1d63eefa546
Alexandre Ramos <natrium.pt@gmail.com>
2021-07-22 16:16:32 +0100

    Added body-parser and cors

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const { json } = require('body-parser');
 
 const app = express();
+
 const port = process.argv[2] || 3000;
 
+app.use(cors());
+app.use(json());
+
 app.all(/.*/, (req, res) => {
-  const { url, method, headers } = req;
-  console.log({ url, method, headers });
-  res.status(200).json({ url, method, headers });
+  const { url, method, headers, body } = req;
+  console.log({ url, method, headers, body });
+  res.status(200).json({ url, method, headers, body });
 });
 
 app.listen(port, () => {
